4e35b2819594aad1469d56137012d538856067fa
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-02 15:09:12 -0400

    calculadora funcional v1

--- calculadora/main.go
+++ calculadora/main.go
@@ -5,36 +5,50 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Escriba sus 2 operadores de la forma: 2+2")
+	fmt.Println("Escriba el primer operador")
 
 	// Lee el imput del usuario
 	scanner.Scan()
 
 	// Convierte el imput a un string y lo guarda en la variable "operacion"
-	operacion := scanner.Text()
+	operador1string := scanner.Text()
 
-	// Separa los numeros cada vez que haya un "+" y los guarda en el array "valores[]"
-	valores := strings.Split(operacion, "+")
+	fmt.Println("Ingrese el tipo de operacion")
 
-	// Convierte "valores[0]" a un numero entero y lo guarda en la variable "operador1"
-	operador1, err1 := strconv.Atoi(valores[0])
-	if err1 != nil {
-		fmt.Println("El primer operador no es un entero")
-	}
+	scanner.Scan()
 
-	// Convierte "valores[1]" a un numero entero y lo guarda en la variable "operador2"
-	operador2, err2 := strconv.Atoi(valores[1])
-	if err2 != nil {
-		fmt.Println("El segundo operador no es un entero")
-	}
+	operador := scanner.Text()
 
-	// Si no hay ningun error en las conversiones imprime la suma de los dos operadores
+	fmt.Println("Ingrese el segundo operador")
+
+	scanner.Scan()
+
+	operador2string := scanner.Text()
+
+	//fmt.Println(operador1string, operador, operador2string)
+
+	operador1, err1 := strconv.Atoi(operador1string)
+
+	operador2, err2 := strconv.Atoi(operador2string)
+
 	if err1 == nil && err2 == nil {
-		fmt.Println(operador1 + operador2)
+		switch operador {
+		case "+":
+			fmt.Println(operador1, operador, operador2, "=", operador1+operador2)
+		case "-":
+			fmt.Println(operador1, operador, operador2, "=", operador1-operador2)
+		case "*":
+			fmt.Println(operador1, operador, operador2, "=", operador1*operador2)
+		case "/":
+			fmt.Println(operador1, operador, operador2, "=", operador1/operador2)
+		default:
+			fmt.Println("Operador invalido")
+		}
+	} else {
+		fmt.Println("Se esperaban numeros enteros")
 	}
 }
