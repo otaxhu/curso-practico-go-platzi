2cbd64c69ff93afb76107c4ac6b4e362710eaee9
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-04 18:15:44 -0400

    middleware, json petitions, handling http methods

--- server-practica/handlers.go
+++ server-practica/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,32 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 
 func HandleAPI(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Api Endpoint =)")
+}
+
+func PostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var metadata Metadata
+	err := decoder.Decode(&metadata)
+	if err != nil {
+		fmt.Fprintf(w, "error %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Data\n%v", metadata)
+}
+
+func UserPostRequest(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "error %v", err)
+		return
+	}
+	res, err := user.ToJson()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
 }
--- server-practica/main.go
+++ server-practica/main.go
@@ -2,7 +2,9 @@ package main
 
 func main() {
 	server := NewServer(":3000")
-	server.Handle("/", HandleRoot)
-	server.Handle("/api", HandleAPI)
+	server.Handle("GET", "/", HandleRoot)
+	server.Handle("POST", "/create", server.AddMiddleware(PostRequest, Logging()))
+	server.Handle("POST", "/user", server.AddMiddleware(UserPostRequest, Logging()))
+	server.Handle("POST", "/api", server.AddMiddleware(HandleAPI, CheckAuth(), Logging()))
 	server.Listen()
 }
