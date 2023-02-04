9cb74585f2d052d1ad1f7589b2566d36c696ac65
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-04 15:24:56 -0400

    router, manejo err 404, imprimo msg a una endpoint

--- server-practica/handlers.go
+++ server-practica/handlers.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hola =)")
+}
+
+func HandleAPI(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Api Endpoint =)")
+}
